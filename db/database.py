9af729e70971c202a66f27d1d7c9556816271c1f
Mathis95c <mathis.renaudin95@gmail.com>
2023-10-31 12:16:47 +0100

    version with query select

--- db/database.py
+++ db/database.py
@@ -27,3 +27,21 @@ def delete(table, id):
     cur = conn.cursor()
     sql = f"DELETE FROM {table} WHERE id = {id};"
     cur.close()
+
+def select(nom_table):
+    try:
+        cursor = conn.cursor()
+        query = f"SELECT * FROM {nom_table};"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+
+    except Exception as e:
+        print(f"Attention ! Erreur : {e}")
+
+    finally:
+        cursor.close()
+
+select("customer_family")
+    
