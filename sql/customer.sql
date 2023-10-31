3a52f567af6773d95183a91fe84c2f646fbfb22d
kisat0 <ghanembenyoucef@gmail.com>
2023-10-31 09:48:20 +0100

    add foreign key table customer

--- sql/customer.sql
+++ sql/customer.sql
@@ -2,5 +2,5 @@ CREATE TABLE customer (
    id VARCHAR(255) PRIMARY KEY,
    code VARCHAR(255) UNIQUE,
    nom STRING,
-   familly STRING,
+   familly STRING FOREIGN KEY REFERENCES customer_familly(id),
 );
