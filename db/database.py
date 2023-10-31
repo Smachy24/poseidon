79f7397272dcf3314346bd7d0ad0ec25b2fd9851
Smachy <pro.diegocdossantos@gmail.com>
2023-10-31 11:12:00 +0100

    constants file and init connection postegres

---
+++ db/database.py
@@ -0,0 +1,5 @@
+import psycopg2
+import constants
+# Connect to your postgres DB
+conn = psycopg2.connect(f"dbname={constants.DBNAME} user={constants.USER} password={constants.PASSWORD}")
+print("test")
