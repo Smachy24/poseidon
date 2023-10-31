9925fffc77cebbfcc2dba395a3576b48e809f568
Smachy <pro.diegocdossantos@gmail.com>
2023-10-31 12:01:54 +0100

    update

--- db/database.py
+++ db/database.py
@@ -4,3 +4,21 @@ import constants
 # Connect to your postgres DB
 conn = psycopg2.connect(f"dbname={constants.DBNAME} user={constants.USER} password={constants.PASSWORD}")
 print("Connection réussie")
+
+def update(table, id, data):
+    cur = conn.cursor()
+    sql = f"UPDATE {table} SET"
+
+    compt = 0
+    for key, value in data.items():
+        if compt<len(data)-1:
+            sql+=f" {key} = {value},"
+        else:
+            sql+=f" {key} = {value}"
+        compt+=1
+
+    sql += f" WHERE id={id};"
+    print(sql)
+    cur.close()
+
+update("client", "test", {"a": "1", "b": "2"})
