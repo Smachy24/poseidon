79cf088c7131f03e576f2512752016af5d7abd2f
kisat0 <ghanembenyoucef@gmail.com>
2023-11-01 16:36:04 +0100

    Add filter on plot asc/desc/limit

--- routers/plot.py
+++ routers/plot.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from pydantic import BaseModel
 from db import database as db
 
@@ -11,8 +11,26 @@ class Plot(BaseModel):
 router = APIRouter()
 
 @router.get("/plots")
-async def get_plots():
-    return db.select("plot")
+async def get_plots(limit: int = Query(None, gt=0), desc: bool = False, asc: bool = True):
+    result = db.select("plot")
+
+    if not isinstance(result, dict) or 'results' not in result:
+        return {'error': 'Invalid data structure for plots'}
+
+    plots = result['results']
+    
+    # Sorting logic based on 'desc' and 'asc'
+    key_to_sort_by = 'plot_number'
+    if desc:
+        plots = sorted(plots, key=lambda x: x.get(key_to_sort_by, 0), reverse=True)
+    elif asc:
+        plots = sorted(plots, key=lambda x: x.get(key_to_sort_by, 0))
+
+    # we verify the input for limit
+    if limit and limit > 0:
+        plots = plots[:limit]
+
+    return {'results': plots}
 
 @router.get("/plots/{plot_number}")
 async def get_plot_by_number(plot_number):
