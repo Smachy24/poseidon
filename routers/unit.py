05d1aea06c2274517da476941ae06ca58a633c63
kisat0 <ghanembenyoucef@gmail.com>
2023-11-02 19:40:42 +0100

    feat/ add auth on unit

--- routers/unit.py
+++ routers/unit.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Depends
 from pydantic import BaseModel
 from db import database as db
+from .user import get_current_user,User
 
 class Unit(BaseModel):
     data: dict
@@ -9,7 +10,7 @@ class Unit(BaseModel):
 router = APIRouter()
 
 @router.get("/units")
-async def get_unit(limit: int = Query(None, gt=0), desc: bool = False, asc: bool = True):
+async def get_unit(limit: int = Query(None, gt=0), desc: bool = False, asc: bool = True, current_user: User = Depends(get_current_user)):
     result = db.select("unit")
 
     if not isinstance(result, dict) or 'results' not in result:
@@ -31,7 +32,7 @@ async def get_unit(limit: int = Query(None, gt=0), desc: bool = False, asc: bool
     return {'results': unit}
 
 @router.get("/units/{unit}")
-async def get_unit_by_unit(unit):
+async def get_unit_by_unit(unit, current_user: User = Depends(get_current_user)):
     """
         Get unit by unit
         @param (int) unit :  unit
@@ -40,7 +41,7 @@ async def get_unit_by_unit(unit):
     return db.select_one("unit", "unit", unit)
 
 @router.post("/units")
-async def create_unit(unit: Unit):
+async def create_unit(unit: Unit, current_user: User = Depends(get_current_user)):
     """
         Insert a new unit
         @param (Unit) unit :  unit got in body
@@ -49,15 +50,15 @@ async def create_unit(unit: Unit):
     return db.insert("unit", unit.data)
 
 @router.put("/units/{unit_value}")
-async def replace_unit(unit_value, unit: Unit):
+async def replace_unit(unit_value, unit: Unit, current_user: User = Depends(get_current_user)):
     return db.update("unit", "unit", unit_value, unit.data)
 
 @router.patch("/units/{unit_value}")
-async def modify_unit(unit_value, unit: Unit):
+async def modify_unit(unit_value, unit: Unit, current_user: User = Depends(get_current_user)):
     return db.update("unit", "unit", unit_value, unit.data)
 
 @router.delete("/units/{unit_value}")
-async def delete_unit(unit_value):
+async def delete_unit(unit_value, current_user: User = Depends(get_current_user)):
     """
         Delete unit by unit value
         @param (str) unit_value :  unit value
