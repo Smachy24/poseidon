b3386478ef0cbfb2289e37ae00ed3e322a4621f8
kisat0 <ghanembenyoucef@gmail.com>
2023-11-02 19:22:35 +0100

    feat/add filters on production

--- routers/production.py
+++ routers/production.py
@@ -8,27 +8,27 @@ class Production(BaseModel):
 
 router = APIRouter()
 
-# @router.get("/plots")
-# async def get_plots(limit: int = Query(None, gt=0), desc: bool = False, asc: bool = True):
-#     result = db.select("plot")
+@router.get("/productions")
+async def get_production(limit: int = Query(None, gt=0), desc: bool = False, asc: bool = True):
+    result = db.select("production")
 
-#     if not isinstance(result, dict) or 'results' not in result:
-#         return {'error': 'Invalid data structure for plots'}
+    if not isinstance(result, dict) or 'results' not in result:
+        return {'error': 'Invalid data structure for production'}
 
-#     plots = result['results']
+    production = result['results']
     
-#     # Sorting logic based on 'desc' and 'asc'
-#     key_to_sort_by = 'plot_number'
-#     if desc:
-#         plots = sorted(plots, key=lambda x: x.get(key_to_sort_by, 0), reverse=True)
-#     elif asc:
-#         plots = sorted(plots, key=lambda x: x.get(key_to_sort_by, 0))
+    # Sorting logic based on 'desc' and 'asc'
+    key_to_sort_by = 'production_code'
+    if desc:
+        production = sorted(production, key=lambda x: x.get(key_to_sort_by, 0), reverse=True)
+    elif asc:
+        production = sorted(production, key=lambda x: x.get(key_to_sort_by, 0))
 
-#     # we verify the input for limit
-#     if limit and limit > 0:
-#         plots = plots[:limit]
+    # we verify the input for limit
+    if limit and limit > 0:
+        production = production[:limit]
 
-#     return {'results': plots}
+    return {'results': production}
 
 @router.get("/productions/{production_code}")
 async def get_production_by_production_code(production_code):
