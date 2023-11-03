c48b61d9b404389ae6a7f93bdf3ca7789bf06572
kisat0 <ghanembenyoucef@gmail.com>
2023-11-03 11:09:47 +0100

    feat/add plot_number filter for spread

--- routers/spread.py
+++ routers/spread.py
@@ -10,7 +10,7 @@ class Spread(BaseModel):
 router = APIRouter()
 
 @router.get("/spreads")
-async def get_spread(limit: int = Query(None, gt=0), desc: bool = False, asc: bool = True, current_user: User = Depends(get_current_user)):
+async def get_spread(limit: int = Query(None, gt=0),plot_number: int = Query(None, gt=0), desc: bool = False, asc: bool = True, current_user: User = Depends(get_current_user)):
     result = db.select("spread")
 
     if not isinstance(result, dict) or 'results' not in result:
@@ -28,6 +28,10 @@ async def get_spread(limit: int = Query(None, gt=0), desc: bool = False, asc: bo
     # we verify the input for limit
     if limit and limit > 0:
         spread = spread[:limit]
+        
+    # we verify the input for plot_number
+    if plot_number and plot_number > 0:
+        spread = [s for s in spread if s['plot_number'] == plot_number]
 
     return {'results': spread}
 
