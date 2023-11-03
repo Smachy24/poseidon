ffb59227369c342fcab80956644d542d1dca2672
Smachy <pro.diegocdossantos@gmail.com>
2023-11-03 12:11:49 +0100

    user file

--- routers/user.py
+++ routers/user.py
@@ -1,121 +1,88 @@
-from fastapi import APIRouter, Query, Depends
+from typing import Annotated
+
+from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
-from db import database as db
-from .user import get_current_user, User
+from db import constants
 
-class Unit(BaseModel):
-
-    """
-    Represents a unit.
-
-    @param (dict) data: Dictionary containing data for the unit.
-    """
-
-    data: dict
-
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+}
 
 router = APIRouter()
 
-@router.get("/units")
-async def get_unit(limit: int = Query(None, gt=0), desc: bool = False, asc: bool = True, current_user: User = Depends(get_current_user)):
-    
-    """
-    Get a list of units.
 
-    @param (int) limit: Maximum number of units to retrieve.
-    @param (bool) desc: Sort in descending order.
-    @param (bool) asc: Sort in ascending order.
-    @param (User) current_user: Current user object obtained from dependency.
+def fake_hash_password(password: str):
+    return "fakehashed" + password
 
-    @return (dict) : Dictionary containing a list of units.
-    """
-    
-    result = db.select("unit")
 
-    if not isinstance(result, dict) or 'results' not in result:
-        return {'error': 'Invalid data structure for unit'}
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f"{constants.TOKEN}")
 
-    unit = result['results']
-    
-    # Sorting logic based on 'desc' and 'asc'
-    key_to_sort_by = 'unit'
-    if desc:
-        unit = sorted(unit, key=lambda x: x.get(key_to_sort_by, 0), reverse=True)
-    elif asc:
-        unit = sorted(unit, key=lambda x: x.get(key_to_sort_by, 0))
 
-    # Verify the input for limit
-    if limit and limit > 0:
-        unit = unit[:limit]
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
 
-    return {'results': unit}
 
-@router.get("/units/{unit}")
-async def get_unit_by_unit(unit, current_user: User = Depends(get_current_user)):
+class UserInDB(User):
+    hashed_password: str
 
-    """
-    Get unit by unit.
 
-    @param (str) unit :  unit.
-    @param (User) current_user: Current user object obtained from dependency.
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
 
-    @return (json) : Message indicating success or error.
-    """
 
-    return db.select_one("unit", "unit", unit)
+def fake_decode_token(token):
+    # This doesn't provide any security at all
+    # Check the next version
+    user = get_user(fake_users_db, token)
+    return user
 
-@router.post("/units")
-async def create_unit(unit: Unit, current_user: User = Depends(get_current_user)):
 
-    """
-    Insert a new unit.
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
 
-    @param (Unit) unit :  unit data.
-    @param (User) current_user: Current user object obtained from dependency.
 
-    @return (json) : Message indicating success or error.
-    """
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
-    return db.insert("unit", unit.data)
 
-@router.put("/units/{unit_value}")
-async def replace_unit(unit_value, unit: Unit, current_user: User = Depends(get_current_user)):
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
 
-    """
-    Replace an existing unit.
+    return {"access_token": user.username, "token_type": "bearer"}
 
-    @param (str) unit_value: Unit value.
-    @param (Unit) unit :  Unit data.
-    @param (User) current_user: Current user object obtained from dependency.
 
-    @return (json) : Message indicating success or error.
-    """
-
-    return db.update("unit", "unit", unit_value, unit.data, {"pk_columns": [], "columns": ["unit"]})
-
-@router.patch("/units/{unit_value}")
-async def modify_unit(unit_value, unit: Unit, current_user: User = Depends(get_current_user)):
-
-    """
-    Modify an existing unit.
-
-    @param (str) unit_value: Unit value.
-    @param (Unit) unit :  Unit data.
-    @param (User) current_user: Current user object obtained from dependency.
-
-    @return (json) : Message indicating success or error.
-    """
-
-    return db.update("unit", "unit", unit_value, unit.data, {"pk_columns": [], "columns": ["unit"]})
-
-@router.delete("/units/{unit_value}")
-async def delete_unit(unit_value, current_user: User = Depends(get_current_user)):
-
-    """
-    Delete unit by unit value.
-
-    @param (str) unit_value :  Unit value.
-    @return (json) : Message indicating success or error.
-    """
-
-    return db.delete("unit", "unit", unit_value)
+@router.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
